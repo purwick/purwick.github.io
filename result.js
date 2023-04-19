b54c478d47a2fc255d43127a6b75a5e4329c6dff
purwick <131195454+purwick@users.noreply.github.com>
2023-04-19 21:17:35 +0000

    Delete result.js

--- result.js
+++
@@ -1,4 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-const score = params.get("score");
-const resultDiv = document.getElementById("result");
-resultDiv.innerText = "Your score is: " + score;
